6fc9d96d5b7c23a2a187ef88c95faa657cf9a16c
yiwei <yiwei@yiwei-Aspire-A315-21>
2018-10-02 12:31:09 -0400

    First Commitment

---
+++ OttwaUniversity/src/test/java/iMetaLab/outline.feature
@@ -0,0 +1,37 @@
+
+
+#Author: yiweis@gmail.com
+#Keywords Summary :
+#Feature: List of scenarios.
+#Scenario: Business rule through list of steps with arguments.
+#Given: Some precondition step
+#When: Some key actions
+#Then: To observe outcomes or validation
+#And,But: To enumerate more Given,When,Then steps
+#Scenario Outline: List of steps for data-driven as an Examples and <placeholder>
+#Examples: Container for s table
+#Background: List of steps run before each of the scenarios
+#""" (Doc Strings)
+#| (Data Tables)
+#@ (Tags/Labels):To group Scenarios
+#<> (placeholder)
+#""
+## (Comments)
+#Sample Feature Definition Template  
+# As bioinformatics student, I want to Launch iMetaLab. 
+ Feature: Scenario iMetaLab.OttawaUniversity
+ 
+ 
+ 	 
+	Scenario: Go to Dashboards
+		Given I have open the browser
+		Given I open iMetaLab website
+		When I click Resource button
+		And I click the DownloadMetaLab button
+		Then The account page shows up
+		When Welcome to iMetaLab shows up
+		And  Create Session button shows up
+		And  View Result button shows up
+		Then And Tutorial button shows up	
+		Then I close the browser
+		
---
+++ OttwaUniversity/src/test/java/iMetaLab/stepdefinition.java
@@ -0,0 +1,110 @@
+package iMetaLab; 
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By; 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver; 
+
+import cucumber.api.java.en.Given; 
+import cucumber.api.java.en.Then; 
+import cucumber.api.java.en.When;
+//import cucumber.api.runtime.PendingException;
+import cucumber.api.java.en.And;
+
+public class stepdefinition { 
+   WebDriver driver = null; 
+
+ 
+
+   @cucumber.api.java.en.Given("I have open the browser")
+   public void I_have_open_the_browser() throws Exception {
+	   System.setProperty("webdriver.gecko.driver", "res/geckodriver");
+
+	   driver = new FirefoxDriver(); 
+	   
+   }  
+
+
+   @Then("I close the browser")
+   public void I_close_the_browser() throws Exception{
+       
+       driver.close();
+   }
+ 
+   @Given("I open iMetaLab website")
+   public void i_open_iMetaLab_website()throws Exception {
+       // Write code here that turns the phrase above into concrete actions
+      driver.get("http://imetalab.ca/");
+   }
+
+   @When("I click Resource button")
+   public void i_click_Resource_button() throws Exception{
+       // Write code here that turns the phrase above into concrete actions
+      driver.findElement(By.xpath("//*[@id=\"nav_0_0-menu\"]/li[3]")).click();
+      Thread.sleep(2000);
+   }
+
+   @When("I click the DownloadMetaLab button")
+   public void i_click_the_DownloadMetaLab_button() throws Exception {
+       // Write code here that turns the phrase above into concrete actions
+	   driver.findElement(By.id("content_4_2-button")).click(); 
+   }
+
+   @Then("The account page shows up")
+   public void the_account_page_shows_up() throws Exception{
+       // Write code here that turns the phrase above into concrete actions
+       try{
+    	   WebElement dash_b=driver.findElement(By.xpath("//span[@class=\"ant-breadcrumb-link\"]"));
+    	   if (dash_b.getText()=="Dashboard") {
+        	   System.out.println("The Dashboard page shows up");
+           }
+           else {
+        	   Exception e;
+           }
+       }catch (Exception e1) {
+    	   System.out.println(e1);
+       }
+       
+   }
+
+   @When("Welcome to iMetaLab shows up")
+   public void welcome_to_iMetaLab_shows_up() throws Exception {
+       // Write code here that turns the phrase above into concrete actions
+      String welcome_text = driver.findElement(By.xpath("//*[@id=\"root\"]/div//span/h3")).getText();
+      String version_text = driver.findElement(By.xpath("//*[@id=\"root\"]/div//span/p")).getText();
+      if (welcome_text=="Welcome to iMetaLab!" && version_text.equalsIgnoreCase("(current version 1.1.1)")) {
+    	        }
+      else {
+    	  System.out.println("The welcome to iMetaLab does not show up correctly!");
+      }
+   }
+
+   @When("Create Session button shows up")
+   public void create_Session_button_shows_up() throws Exception{
+       // Write code here that turns the phrase above into concrete actions
+       driver.findElements(By.xpath("//*[@id=\"root\"]/div//a/div[1]")).get(0);
+   }
+
+   @When("View Result button shows up")
+   public void view_Result_button_shows_up() throws Exception{
+       // Write code here that turns the phrase above into concrete actions
+	   driver.findElements(By.xpath("//*[@id=\"root\"]/div//a/div[1]")).get(1);
+   }
+
+   @Then("And Tutorial button shows up")
+   public void and_Tutorial_button_shows_up() throws Exception{
+       // Write code here that turns the phrase above into concrete actions
+	   driver.findElements(By.xpath("//*[@id=\"root\"]/div//a/div[1]")).get(2);
+   }
+
+   @Then("I close the browser")
+   public void i_close_the_browser() throws Exception{
+       // Write code here that turns the phrase above into concrete actions
+	   driver.close();
+   }
+
+ 
+}
