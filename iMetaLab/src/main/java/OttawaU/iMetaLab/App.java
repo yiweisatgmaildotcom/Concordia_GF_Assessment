f5f68a3bc175b4cf23bdae5209f4b832cf6fcfa3
yiwei <yiwei@yiwei-Aspire-A315-21>
2018-10-03 09:41:33 -0400

    First Commit

---
+++ iMetaLab/src/main/java/OttawaU/iMetaLab/App.java
@@ -0,0 +1,13 @@
+package OttawaU.iMetaLab;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
