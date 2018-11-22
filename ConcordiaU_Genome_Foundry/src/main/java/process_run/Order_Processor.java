ed44954696c87a2f899e9aad2baf20865b343337
yiwei <yiwei@yiwei-Aspire-A315-21>
2018-11-22 08:17:51 -0500

    Added design graph as pdf from LucidArt and Order_Process.java

--- ConcordiaU_Genome_Foundry/src/main/java/process_run/Order_Processor.java
+++ ConcordiaU_Genome_Foundry/src/main/java/process_run/Order_Processor.java
@@ -1,5 +1,12 @@
 package process_run;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Order_Processor {
 	
 	
@@ -11,12 +18,22 @@ public class Order_Processor {
 	 * This method will deal with the order one by one.
 	 */
 	public void order_process() {
+		//sort the orders
+		Map <String, ArrayList<String>> dict = new HashMap<String, ArrayList<String>>();
+		Map <String, ArrayList<String>> dict_tree;
 		for (int order_ind=0; order_ind<a_reader.getOrders().size(); order_ind++) {
 			Tokenizer a_toke= new Tokenizer(a_reader.getOrders().get(order_ind));
+			ArrayList<String> order_value = new ArrayList<String>();
+			for(int i=1;i<a_toke.result.length;i++) {
+				order_value.add(a_toke.result[i]);
+			}
 			
+			dict.put(a_toke.result[0], order_value);
 			
 			
 		}
+		//to check if the treeMap is ordered faster than HashMap? 
+		dict_tree = new TreeMap<String, ArrayList<String>>(dict);
 	}
 	
 }
