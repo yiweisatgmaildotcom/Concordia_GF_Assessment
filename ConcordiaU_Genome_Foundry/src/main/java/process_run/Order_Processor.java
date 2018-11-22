9f7b8a6a2d472c141f38f0ad39ba8e3135e24e30
yiwei <yiwei@yiwei-Aspire-A315-21>
2018-11-22 11:46:16 -0500

    Added the changes in Order_Processor.java and Tokenizer.java

--- ConcordiaU_Genome_Foundry/src/main/java/process_run/Order_Processor.java
+++ ConcordiaU_Genome_Foundry/src/main/java/process_run/Order_Processor.java
@@ -3,23 +3,33 @@ package process_run;
 import java.util.ArrayList;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class Order_Processor {
 	
-	
 	CSV_Reader a_reader;
+	ArrayList<String> filledOrder;
+	LinkedList<String>noFilledOrder;
+	Map <String, ArrayList<String>> dict;
+	Map <String, LinkedList<String>> no_dict;
+	Map <String, ArrayList<String>> dict_tree ;
 	public Order_Processor(CSV_Reader a_reader) {
 		this.a_reader = a_reader; 
+		filledOrder = new ArrayList<String>();
+		noFilledOrder = new LinkedList<String>();
+		dict = new HashMap<String, ArrayList<String>>();
+		no_dict = new HashMap<String, LinkedList<String>>();
+		dict_tree = new TreeMap<String, ArrayList<String>>();
+		
 	}
 	/**
 	 * This method will deal with the order one by one.
 	 */
 	public void order_process() {
 		//sort the orders
-		Map <String, ArrayList<String>> dict = new HashMap<String, ArrayList<String>>();
-		Map <String, ArrayList<String>> dict_tree = new TreeMap<String, ArrayList<String>>(dict);;
+		
 		for (int order_ind=0; order_ind<a_reader.getOrders().size(); order_ind++) {
 			Tokenizer a_toke= new Tokenizer(a_reader.getOrders().get(order_ind));
 			ArrayList<String> order_value = new ArrayList<String>();
@@ -41,7 +51,68 @@ public class Order_Processor {
 		for(String key: dict_tree.keySet()) {
 			System.out.println("The key, value in dict_tree is " + key + " "+dict_tree.get(key));			
 		}
-		//compare with the inventory volumn 
+		//order process by order numbers
+		for(String key: dict_tree.keySet()) {
+			dict.clear();
+			System.out.println("Dealing with the order " + key + "... ");
+			for (String item: dict_tree.get(key)){
+				//if item is matched and volumn is not zero
+				boolean filled = false;
+				for(int j=0; j<a_reader.getInv_list().size(); j++) {
+					String an_item = a_reader.getInv_list().get(j).getItem();
+					String a_vol = a_reader.getInv_list().get(j).getVolumn();
+					if(item.equalsIgnoreCase(an_item)& Integer.parseInt(a_vol)>0) {
+						filled= true;
+						a_reader.getInv_list().get(j).setVolumnMinusOne(a_vol);						
+					}
+					else 
+						noFilledOrder.add(item);
+					if (filled){
+						filledOrder.add(item);
+						filled= false;
+					}					
+				}
+				
+			}
+			dict.put(key, filledOrder);
+			no_dict.put(key, noFilledOrder);
+		}//for loop for keys in dict_tree 
+	}
+/////////////////////////////////////////////////////////////////////////////////////////
+// setters or getters
+/////////////////////////////////////////////////////////////////////////////////////////
+	public ArrayList<String> getFilledOrder() {
+		return filledOrder;
+	}
+	public void setFilledOrder(ArrayList<String> filledOrder) {
+		this.filledOrder = filledOrder;
+	}
+	public LinkedList<String> getNoFilledOrder() {
+		return noFilledOrder;
+	}
+	public void setNoFilledOrder(LinkedList<String> noFilledOrder) {
+		this.noFilledOrder = noFilledOrder;
+	}
+	public Map<String, ArrayList<String>> getDict() {
+		return dict;
+	}
+	public void setDict(Map<String, ArrayList<String>> dict) {
+		this.dict = dict;
+	}
+	public Map<String, LinkedList<String>> getNo_dict() {
+		return no_dict;
+	}
+	public void setNo_dict(Map<String, LinkedList<String>> no_dict) {
+		this.no_dict = no_dict;
+	}
+	public Map<String, ArrayList<String>> getDict_tree() {
+		return dict_tree;
+	}
+	public void setDict_tree(Map<String, ArrayList<String>> dict_tree) {
+		this.dict_tree = dict_tree;
+	}
+	public CSV_Reader getA_reader() {
+		return a_reader;
 	}
 	
 }
--- ConcordiaU_Genome_Foundry/src/main/java/process_run/Tokenizer.java
+++ ConcordiaU_Genome_Foundry/src/main/java/process_run/Tokenizer.java
@@ -16,12 +16,22 @@ public class Tokenizer {
         }
     }
    
-    
+    /**
+     * This method will return the integer of volumn according to the token position.
+     * @param position
+     * @return
+     */
     public int volumn(int position)
     {
         String temp[] = result[position].split(",");
         return Integer.parseInt(temp[3]);
     }
+    /**
+     * This method will get order number according to the result positon, it is normally 0
+     * in the order lines.
+     * @param position
+     * @return
+     */
     public int order_num(int position) {
     	//String temp[] = result[0];
     	String order_str = result[position];
