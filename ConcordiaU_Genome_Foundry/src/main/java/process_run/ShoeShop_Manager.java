cf1b159013cbd2a533481726fbe6331c6be4d791
yiwei <yiwei@yiwei-Aspire-A315-21>
2018-11-23 08:14:52 -0500

    UPDATE the ShoeShop_Manager.java comments according to flowchart design..

--- ConcordiaU_Genome_Foundry/src/main/java/process_run/ShoeShop_Manager.java
+++ ConcordiaU_Genome_Foundry/src/main/java/process_run/ShoeShop_Manager.java
@@ -3,16 +3,16 @@ package process_run;
 public class ShoeShop_Manager {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// TODO preparation, 
 		
-		// TODO input with csv reader
+		// TODO preprocess, 
+		// Cleaning up the *.csv files, get rid of extra "" and useless info.
+		// INPUT with csv reader
 		CSV_Reader a_reader= new CSV_Reader();
 		a_reader.start_reading();
-		// TODO pre and process, dealing with order		
+		// PROCESS, dealing with order		
 		Order_Processor ao_pr = new Order_Processor(a_reader);
 		ao_pr.order_process();
-		// TODO output the two files output.csv and inventory.csv with csv writer
+		// OUTPUT the three files output.csv, NoFilledOrder.csv and inventory.csv with csv writer
 		CSV_Writor a_wtr = new CSV_Writor(ao_pr,a_reader);
 		a_wtr.write();
 		
